aa498edaee821fe827701b355f90bbfdb825b7ba
gilvan <g.r.almeida@live.com>
2019-08-11 19:44:17 -0300

    Adding the searchVideo action

---
+++ src/redux/store/actions/search-video.js
@@ -0,0 +1,39 @@
+import youtubeSearch from 'youtube-api-v3-search'
+
+import API_KEY from 'api'
+import {
+  START_VIDEO_SEARCH,
+  SUCCESS_VIDEO_SEARCH,
+  ERROR_VIDEO_SEARCH
+} from './constants'
+
+export const startVideoSearch = () => {
+  return {
+    type: START_VIDEO_SEARCH,
+    isLoading: true,
+    error: false
+  }
+}
+
+export const successVideoSearch = videos => {
+  return {
+    type: SUCCESS_VIDEO_SEARCH
+  }
+}
+
+export const errorVideoSearch = () => {
+  return {
+    type: ERROR_VIDEO_SEARCH,
+    isLoading: false,
+    error: true
+  }
+}
+
+export const searchVideo = term => {
+  return dispatch => {
+    dispatch(startVideoSearch)
+    youtubeSearch(API_KEY, { q: term })
+      .then(data => dispatch(successVideoSearch(data.item)))
+      .catch(() => dispatch(errorVideoSearch))
+  }
+}
