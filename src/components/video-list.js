91c7d6133b395d1a173b1c4da0f0d43be6799b14
gilvan <g.r.almeida@live.com>
2019-08-16 22:51:59 -0300

    Displaying videos in the VideoList component

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { List, Image } from 'semantic-ui-react'
 
 const VideoList = () => (
   <div className='video-list'>
-    <h1>VideoList</h1>
+    <List
+      animated
+      verticalAlign='middle'
+    >
+      <List.Item>
+        <Image
+          avatar
+          src=''
+        />
+        <List.Content>
+          <List.Header>VideoTitle</List.Header>
+        </List.Content>
+      </List.Item>
+    </List>
   </div>
 )
 
