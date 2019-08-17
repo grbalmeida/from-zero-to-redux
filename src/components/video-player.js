90631179343c05242930a0e57843ccdb0c64795a
gilvan <g.r.almeida@live.com>
2019-08-17 00:17:14 -0300

    Playing the youtube video

--- src/components/video-player.js
+++ src/components/video-player.js
@@ -1,16 +1,37 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Advertisement } from 'semantic-ui-react'
+import { Advertisement, Embed } from 'semantic-ui-react'
 
-const VideoPlayer = () => (
-  <div className='video-player'>
-    <Advertisement
-      style={{ height: '433px' }}
-      unit='top banner'
-      test='Choose a video to play'
-    />
-  </div>
-)
+const VideoPlayer = ({ video }) => {
+  const { id, title, description } = video
+
+  return (
+    <div className='video-player'>
+      {!id &&
+        <Advertisement
+          style={{ height: '433px' }}
+          unit='top banner'
+          test='Choose a video to play'
+        />
+      }
+      {id &&
+        <div>
+          <Embed
+            id={id.videoId}
+            source='youtube'
+          />
+          <p>{title}</p>
+          <p>{description}</p>
+        </div>
+      }
+    </div>
+  )
+}
+
+VideoPlayer.propTypes = {
+  video: PropTypes.object.isRequired
+}
 
 const mapStateToProps = state => {
   return {
