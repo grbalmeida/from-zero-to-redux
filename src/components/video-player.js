a339900367482f6e0282d1d231d11c9df31e29f8
gilvan <g.r.almeida@live.com>
2019-08-17 00:02:11 -0300

    Starting the video player

--- src/components/video-player.js
+++ src/components/video-player.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Advertisement } from 'semantic-ui-react'
 
 const VideoPlayer = () => (
   <div className='video-player'>
-    <h1>VideoPlayer</h1>
+    <Advertisement
+      style={{ height: '433px' }}
+      unit='top banner'
+      test='Choose a video to play'
+    />
   </div>
 )
 
-export default VideoPlayer
+const mapStateToProps = state => {
+  return {
+    video: state.playVideoReducer.video
+  }
+}
+
+export default connect(mapStateToProps, null)(VideoPlayer)
