52c991da9386bb88b5d3935af6de8d96416bb00f
gilvan <g.r.almeida@live.com>
2019-08-16 21:58:40 -0300

    Connecting the project to Redux with Provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 
+import store from 'redux/store'
 import { SearchBar, VideoList, VideoPlayer } from 'components'
 import './App.css'
 
 const App = () => (
-  <div className='container'>
-    <SearchBar />
-    <VideoList />
-    <VideoPlayer />
-  </div>
+  <Provider store={store}>
+    <div className='container'>
+      <SearchBar />
+      <VideoList />
+      <VideoPlayer />
+    </div>
+  </Provider>
 )
 
 export default App
