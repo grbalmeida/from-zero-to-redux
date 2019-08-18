1149e085d870fbd8538feb9ceebd8081b57ebc9f
gilvan <g.r.almeida@live.com>
2019-08-18 01:46:42 -0300

    Adding Material UI to the project

--- shopping-list/src/App.js
+++ shopping-list/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { createMuiTheme } from '@material-ui/core/styles'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+
+import './App.css'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#E91E63'
+    },
+    secondary: {
+      main: '#00B0FF'
+    }
+  }
+})
 
 const App = () => (
-  <div>App</div>
+  <MuiThemeProvider theme={theme}>
+    <div>App</div>
+  </MuiThemeProvider>
 )
 
 export default App
