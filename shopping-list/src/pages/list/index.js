62804746293915e5370d53ff2857f799b03b501a
gilvan <g.r.almeida@live.com>
2019-08-18 16:03:06 -0300

    Styling the form

--- shopping-list/src/pages/list/index.js
+++ shopping-list/src/pages/list/index.js
@@ -1,57 +1,65 @@
 import React from 'react'
 import {
+  Button,
   InputAdornment,
   MenuItem,
   TextField
 } from '@material-ui/core'
+
+import './List.css'
 
 const units = ['kg', 'lt', 'un']
 
 const List = () => (
   <div className='page-container'>
     <form className='form-container'>
-      <TextField
-        label='List'
-        name='list'
-        value=''
-        onChange={() => {}}
-        required
-      />
-      <TextField
-        label='Product'
-        name='product'
-        value=''
-        onChange={() => {}}
-        required
-      />
-      <TextField
-        label='Quantity'
-        name='quantity'
-        value=''
-        onChange={() => {}}
-        required
-      />
-      <TextField
-        select
-        label='Unit'
-        name='unit'
-        value=''
-        onChange={() => {}}
-        required
-      >
-        {units.map(unit => (
-          <MenuItem key={unit} value={unit}>{unit}</MenuItem>
-        ))}
-      </TextField>
-      <TextField
-        label='Price'
-        name='price'
-        value=''
-        onChange={() => {}}
-        InputProps={{
-          startAdornment: <InputAdornment position='start'>R$</InputAdornment>
-        }}
-      />
+      <div className='form-row'>
+        <TextField
+          label='List'
+          name='list'
+          value=''
+          onChange={() => {}}
+          required
+        />
+        <Button variant='outlined' color='secondary'>Add</Button>
+      </div>
+      <div className='form-row'>
+        <TextField
+          label='Product'
+          name='product'
+          value=''
+          onChange={() => {}}
+          required
+        />
+        <TextField
+          label='Quantity'
+          name='quantity'
+          value=''
+          onChange={() => {}}
+          required
+        />
+        <TextField
+          select
+          label='Unit'
+          name='unit'
+          value=''
+          onChange={() => {}}
+          required
+        >
+          {units.map(unit => (
+            <MenuItem key={unit} value={unit}>{unit}</MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          label='Price'
+          name='price'
+          value=''
+          onChange={() => {}}
+          InputProps={{
+            startAdornment: <InputAdornment position='start'>R$</InputAdornment>
+          }}
+        />
+      </div>
     </form>
   </div>
 )
