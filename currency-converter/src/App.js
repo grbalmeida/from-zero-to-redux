6b5eff2a61a90555c2e348d5eecc15822c3098fb
gilvan <g.r.almeida@live.com>
2019-08-23 09:53:38 -0300

    Adding Header component markup

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import 'bootstrap-css-only'
 import { Header } from 'components'
 
 const App = () => (
--- currency-converter/src/components/header.js
+++ currency-converter/src/components/header.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const Header = () => (
-  <h1>Header</h1>
+  <div className='jumbotron'>
+    <h1 className='display-4'>Currency Converter</h1>
+    <p className='lead'>From zero to Redux</p>
+  </div>
 )
 
 export default Header
