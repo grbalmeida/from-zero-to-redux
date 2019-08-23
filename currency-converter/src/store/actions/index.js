b105de6f6e49618754fde066a7d63571c45a0427
gilvan <g.r.almeida@live.com>
2019-08-23 14:57:12 -0300

    Creating the action convertCurrencies

---
+++ currency-converter/src/store/actions/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+import { CONVERT_CURRENCIES } from './constants'
+import API_KEY from 'api-key-currency'
+
+const BASE_URL = 'http://www.apilayer.net/api/'
+
+export function convertCurrencies (from, to, amount) {
+	const url = `${BASE_URL}convert?access_key=${API_KEY}`
+
+	const parameters = {
+		from,
+		to,
+		amount,
+		format: 1
+	}
+
+	const request = axios.get(url, parameters)
+
+	return {
+		type: CONVERT_CURRENCIES,
+		payload: request
+	}
+}
